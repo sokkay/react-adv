3141e4b706ea361d5d0b9e21b2825eedd82e4fef
Carlos Mondaca <carmond518@gmail.com>
2021-12-18 12:42:36 -0300

    Componentes agregados

---
+++ src/01-lazyload/pages/LazyPage.tsx
@@ -0,0 +1,7 @@
+export const LazyPage = () => {
+  return (
+    <h1>
+      Lazy Page 1
+    </h1>
+  )
+}
---
+++ src/01-lazyload/pages/LazyPage2.tsx
@@ -0,0 +1,7 @@
+export const LazyPage2 = () => {
+  return (
+    <h1>
+      Lazy Page 1
+    </h1>
+  )
+}
